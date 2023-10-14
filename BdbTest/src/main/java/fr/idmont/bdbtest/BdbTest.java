48a75ec8f1f27257cd8525ba2ff9bb8dc652a2d5
cidmo <cidmo@DESKTOP-DRC4MLL>
2023-10-14 21:50:46 +0200

    FIN TP1

--- BdbTest/src/main/java/fr/idmont/bdbtest/BdbTest.java
+++ BdbTest/src/main/java/fr/idmont/bdbtest/BdbTest.java
@@ -63,7 +63,7 @@ public class BdbTest {
         try (Statement st = con.createStatement()) {
             ResultSet res = st.executeQuery("SELECT * FROM Tmachine ");
             while (res.next()) {
-                System.out.println(res.getInt(1) + " " + res.getString(2) + " " + res.getString(3)+" "+res.getInt(4));
+                System.out.println(res.getInt(1) + " " + res.getString(2) + " " + res.getString(3) + " " + res.getInt(4));
             }
             System.out.println("OK");
         } catch (SQLException ex) {
@@ -84,10 +84,75 @@ public class BdbTest {
                     + "    puissance integer not null"
                     + ")\n"
             );
-            System.out.println("Table crée");
+            st.executeUpdate(
+                    "create table Toperations (\n"
+                    + "    id integer not null primary key AUTO_INCREMENT,\n"
+                    + "    idtype integer not null ,\n"
+                    + "    id_produit integer not null\n"
+                    + ")\n"
+            );
+            st.executeUpdate(
+                    "create table Tprecedence (\n"
+                    + "    opavant integer not null,\n"
+                    + "    opapres integer not null\n"
+                    + ")\n"
+            );
+            st.executeUpdate(
+                    "create table Tproduits (\n"
+                    + "    id integer not null primary key AUTO_INCREMENT,\n"
+                    + "    ref varchar(30) not null unique,\n"
+                    + "    des varchar(30) not null\n"
+                    + ")\n"
+            );
+            st.executeUpdate(
+                    "create table Trealise (\n"
+                    + "    id_machine integer not null,\n"
+                    + "    id_type integer not null,\n"
+                    + "    duree float not null\n"
+                    + ")\n"
+            );
+            st.executeUpdate(
+                    "create table Ttype_operation (\n"
+                    + "    id integer not null primary key AUTO_INCREMENT,\n"
+                    + "    des varchar(30) not null\n"
+                    + ")\n"
+            );
+            st.executeUpdate(
+                    "alter table Toperations  \n"
+                    + "    add constraint fk_Toperations_produits \n"
+                    + "    foreign key (id_produit) references Tproduits(id) \n"
+            );
+            st.executeUpdate(
+                    "alter table Toperations  \n"
+                    + "    add constraint fk_Toperations_typeoperations \n"
+                    + "    foreign key (idtype) references Ttype_operation(id) \n"
+            );
+            st.executeUpdate(
+                    "alter table Tprecedence  \n"
+                    + "    add constraint fk_Tprecedence_opavant \n"
+                    + "    foreign key (opavant) references Toperations(id) \n"
+            );
+            st.executeUpdate(
+                    "alter table Tprecedence  \n"
+                    + "    add constraint fk_Tprecedence_opapres \n"
+                    + "    foreign key (opapres) references Toperations(id) \n"
+            );
+            st.executeUpdate(
+                    "alter table Trealise  \n"
+                    + "    add constraint Tfk_realise_idmachine \n"
+                    + "    foreign key (id_machine) references Tmachine(id) \n"
+            );
+            st.executeUpdate(
+                    "alter table Trealise  \n"
+                    + "    add constraint Tfk_realise_idtype \n"
+                    + "    foreign key (id_type) references Ttype_operation(id) \n"
+            );
+
+            con.commit();
+            System.out.println("Schema crée !");
 
         } catch (SQLException ex) {
-            System.out.println("Table déja existante");
+            System.out.println("Scema deja existant.");
             con.rollback();
         } finally {
             con.setAutoCommit(true);
@@ -98,11 +163,21 @@ public class BdbTest {
     public static void drop(Connection con) throws SQLException {
 
         try (Statement st = con.createStatement()) {
-            st.executeUpdate(
-                    "drop table Tmachine"
-            );
-        } catch (SQLException ex) {
-        }
+            
+            try {st.executeUpdate("alter table Toperations drop constraint fk_Toperations_produits");} catch(SQLException ex){}
+            try {st.executeUpdate("alter table Toperations drop constraint fk_Toperations_typeoperations");} catch(SQLException ex){}
+            try {st.executeUpdate("alter table Tprecedence drop constraint fk_Tprecedence_opavant");} catch(SQLException ex){}
+            try {st.executeUpdate("alter table Tprecedence drop constraint fk_Tprecedence_opapres");} catch(SQLException ex){}
+            try {st.executeUpdate("alter table Trealise drop constraint Tfk_realise_idmachine");} catch(SQLException ex){}
+            try {st.executeUpdate("alter table Trealise drop constraint Tfk_realise_idtype");} catch(SQLException ex){}
+           
+            try {st.executeUpdate("drop table Tmachine");} catch(SQLException ex){}
+            try {st.executeUpdate("drop table Toperations");} catch(SQLException ex){}
+            try {st.executeUpdate("drop table Tprecedence");} catch(SQLException ex){}
+            try {st.executeUpdate("drop table Tproduits");} catch(SQLException ex){}
+            try {st.executeUpdate("drop table Trealise");} catch(SQLException ex){}
+            try {st.executeUpdate("drop table Ttype_operation");} catch(SQLException ex){}
+        } 
 
     }
 
@@ -114,13 +189,13 @@ public class BdbTest {
         int port = 3306;
         String pw = "52686b49";
 
-        Connection con = connectionMySQL(host, 3306, db, user, pw);
+        Connection con = connectionMySQL(host, port, db, user, pw);
 
         boolean fin = true;
         while (fin) {
             System.out.println("Que voulez-vous faire?");
-            System.out.println("1) Créer table :");
-            System.out.println("2) Supprimer table :");
+            System.out.println("1) Créer schema :");
+            System.out.println("2) Supprimer schema :");
             System.out.println("3) Afficher table :");
             System.out.println("4) Ajouter machine :");
             System.out.println("5) Stop :");
